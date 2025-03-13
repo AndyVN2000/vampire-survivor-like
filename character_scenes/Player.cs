5a6a8e592e7e22bdd1c09f00ba60b2a722a79b97
andy <andyvu@outlook.dk>
2025-03-13 15:06:16 +0100

    Added player movement

---
+++ character_scenes/Player.cs
@@ -0,0 +1,45 @@
+using Godot;
+using System;
+
+public partial class Player : Area2D
+{
+
+	[Export]
+	public int speed { get; set; } = 400;
+	
+	public override void _Ready()
+	{}
+	
+	public override void _Process(double delta)
+	{
+		
+		var velocity = Vector2.Zero; // Player's movement vector
+		
+		// Read the movement inputs
+		if (Input.IsActionPressed("move_right"))
+		{
+			velocity.X += 1;
+		}
+		if (Input.IsActionPressed("move_left"))
+		{
+			velocity.X -= 1;
+		}
+		if (Input.IsActionPressed("move_up"))
+		{
+			velocity.Y -= 1;
+		}
+		if (Input.IsActionPressed("move_down"))
+		{
+			velocity.Y += 1;
+		}
+		
+		if (velocity.Length() > 0)
+		{
+			// Movement speed is normalized
+			velocity = velocity.Normalized() * speed;
+		}
+		
+		Position += velocity * (float)delta;
+	}
+	
+}
