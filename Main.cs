3c282b01626d1770919672eaa7908f9aaa0a32ae
andy <andyvu@outlook.dk>
2025-03-15 22:52:47 +0100

    Mobs are spawning, but spawn area does not follow camera yet

---
+++ Main.cs
@@ -0,0 +1,30 @@
+using Godot;
+using System;
+
+public partial class Main : Node
+{
+	PackedScene EnemyScene = GD.Load<PackedScene>("res://character_scenes/enemy_scenes/Mob.tscn");
+	
+	
+	public override void _Ready()
+	{
+		GetNode<Timer>("SpawnArea/EnemySpawnTimer").Start();
+	}
+	
+	
+	
+	private void OnEnemySpawnTimerTimeout()
+	{
+		GD.Print("Enemy spawned");
+		
+		Node2D enemy = EnemyScene.Instantiate<Node2D>();
+		
+		var enemySpawnLocation = GetNode<PathFollow2D>("SpawnArea/SpawnPath/EnemySpawnLocation");
+		enemySpawnLocation.ProgressRatio = GD.Randf();
+		
+		enemy.Position = enemySpawnLocation.Position;
+		
+		AddChild(enemy);
+		
+	}
+}
