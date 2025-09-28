1fada20f8566d54b3c6afbf3dfc550f10e05c886
andy <andyvu@outlook.dk>
2025-09-28 23:30:26 +0200

    Added a Projectile scene

--- weapons/BaseWeapon.cs
+++ weapons/BaseWeapon.cs
@@ -1,17 +1,26 @@
 using Godot;
 using System;
 using static Enemy;
+using static Projectile;
 
 public abstract partial class BaseWeapon : Node
 {
-	private string name;
-	private int range;
-	private float damage;
-	private float cooldown;
+	
+	private string _name;
+	[Export] private PackedScene _projectile;
+	private int _range;
+	private float _damage;
+	private float _cooldown;
+	
 	
 	public virtual Enemy ComputeEnemyTarget()
 	{
 		return null;
+	}
+	
+	public void Shoot()
+	{
+		_projectile.Instantiate<Projectile>();
 	}
 	
 }
--- weapons/Projectile.cs
+++ weapons/Projectile.cs
@@ -1,6 +1,7 @@
 using Godot;
 using System;
 
-public abstract partial class Projectile : Node
+public abstract partial class Projectile : Area2D
 {
+	
 }
