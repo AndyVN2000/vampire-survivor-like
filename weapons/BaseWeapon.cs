a8505ed58ac83a59a358333e3d71a089dacb631c
andy <andyvu@outlook.dk>
2025-09-28 23:56:08 +0200

    Added code to BaseWeapon

--- weapons/BaseWeapon.cs
+++ weapons/BaseWeapon.cs
@@ -4,16 +4,38 @@ using static Projectile;
 
 public abstract partial class BaseWeapon : Node
 {
+	public Player Owner { get; private set; }
 	
 	private string _name;
-	[Export] private PackedScene _projectile;
+	[Export] private PackedScene _projectileScene;
 	private int _range;
 	private float _damage;
 	private float _cooldown;
+	private float _timer;
+	
+	public override void _Ready()
+	{}
+	
+	public override void _Process(double delta)
+	{
+		_timer -= (float)delta;
+		if (_timer <= 0)
+		{
+			Shoot();
+			_timer = _cooldown;
+		}
+	}
+	
+	public void Initialize(Player player)
+	{
+		Owner = player;
+	}
 	
 	public void Shoot()
 	{
-		_projectile.Instantiate<Projectile>();
+		var projectile = _projectileScene.Instantiate<Projectile>();
+		projectile.GlobalPosition = Owner.GlobalPosition;
+		GetTree().CurrentScene.AddChild(projectile);
 	}
 	
 }
