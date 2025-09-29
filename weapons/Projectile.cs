df01d2153f2a4596ccc21a98781641f57b62c903
andy <andyvu@outlook.dk>
2025-09-29 16:07:58 +0200

    Added Process function to Projectile

--- weapons/Projectile.cs
+++ weapons/Projectile.cs
@@ -4,11 +4,17 @@ using System;
 public abstract partial class Projectile : Area2D
 {
 	private Vector2 Direction;
-	private float speed;
-	
+	private float _speed;
+
 	public virtual void Initialize(Vector2 startPosition, Vector2 direction)
 	{
 		GlobalPosition = startPosition;
 		Direction = direction.Normalized();
 	}
+
+	public override void _Process(double delta)
+	{
+		GlobalPosition += Direction * _speed * (float)delta;
+	}
+	
 }
