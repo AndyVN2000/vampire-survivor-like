184860e889236889f64c79fa2023d271f4aa41c4
andy <andyvu@outlook.dk>
2025-09-29 16:01:34 +0200

    Added projectile assets

--- weapons/Projectile.cs
+++ weapons/Projectile.cs
@@ -3,5 +3,12 @@ using System;
 
 public abstract partial class Projectile : Area2D
 {
+	private Vector2 Direction;
+	private float speed;
 	
+	public virtual void Initialize(Vector2 startPosition, Vector2 direction)
+	{
+		GlobalPosition = startPosition;
+		Direction = direction.Normalized();
+	}
 }
