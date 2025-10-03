b400feef263618b264551f51687a6bdf32898195
andy <andyvu@outlook.dk>
2025-10-03 15:38:43 +0200

    WIP making projectile move towards closest enemy

--- weapons/BasicProjectile.cs
+++ weapons/BasicProjectile.cs
@@ -5,16 +5,22 @@ public partial class BasicProjectile : Projectile
 {
 
 	AnimatedSprite2D _sprite;
+	TargetingUtil _targetingUtil;
+	Vector2 _direction;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		_sprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
 		_sprite.Play();
+		_targetingUtil = new TargetingUtil();
+		Vector2 targetPosition = _targetingUtil.ComputeTargetPosition(this.Position);
+		_direction = Position.DirectionTo(targetPosition);
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		Position += _direction * (float)delta;
 	}
 }
--- weapons/utils/TargetingUtil.cs
+++ weapons/utils/TargetingUtil.cs
@@ -1,13 +1,35 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 using static Enemy;
 
 public partial class TargetingUtil : Node
 {
-	
-	public Enemy ComputeEnemyTarget()
+
+	public void _Ready()
 	{
-		return null;
+	}
+
+	public Node2D ComputeClosestEnemy(Vector2 currentPosition)
+	{
+		var enemies = GetTree().GetNodesInGroup("EnemiesInRange");
+		Node2D closestEnemy = null;
+		float closesDistance = float.MaxValue;
+		foreach (Node2D enemy in enemies)
+		{
+			float distance = currentPosition.DistanceSquaredTo(enemy.Position);
+			if (closestEnemy == null || distance < closesDistance)
+			{
+				closestEnemy = enemy;
+			}
+
+		}
+		return closestEnemy;
+	}
+
+	public Vector2 ComputeTargetPosition(Vector2 currentPosition)
+	{
+		return ComputeClosestEnemy(currentPosition).Position;
 	}
 	
 }
