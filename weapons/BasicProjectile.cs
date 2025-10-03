51087da057ab1cb60d0e1e69f3505e95edcb314e
andy <andyvu@outlook.dk>
2025-10-03 15:06:54 +0200

    Changed the projectile asset

--- weapons/BasicProjectile.cs
+++ weapons/BasicProjectile.cs
@@ -3,9 +3,14 @@ using System;
 
 public partial class BasicProjectile : Projectile
 {
+
+	AnimatedSprite2D _sprite;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		_sprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+		_sprite.Play();
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
